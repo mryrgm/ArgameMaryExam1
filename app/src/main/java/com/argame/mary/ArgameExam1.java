63eeb01ea1273c6414cfb471655b3757d66cb43a
mryrgm <maryruth.argame.iics@ust.edu.ph>
2018-10-06 09:58:25 +0800

    Initial commit

---
+++ app/src/main/java/com/argame/mary/ArgameExam1.java
@@ -0,0 +1,13 @@
+package com.argame.mary;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ArgameExam1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_argame_exam1);
+    }
+}
